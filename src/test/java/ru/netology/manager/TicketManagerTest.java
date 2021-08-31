1294a43167641c3cdd3ee3e75aa7a378fe636c61
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-31 19:52:02 +0700

    Update TicketManagerTest.java

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -18,30 +18,57 @@ class TicketManagerTest {
     private Ticket t3 = new Ticket(3, 8000, "UR", "UMK", 90);
     private Ticket t4 = new Ticket(4, 11000, "UMK", "UW", 220);
     private Ticket t5 = new Ticket(5, 13000, "UMK", "UW", 150);
-    private Ticket t6 = new Ticket(6, 15000, "RUS", "RUS", 240);
-    private Ticket t7 = new Ticket(7, 17000, "RUS", "RUS", 720);
-    private Ticket t8 = new Ticket(8, 16000, "RUS", "RUS", 130);
-    private Ticket t9 = new Ticket(9, 14000, "RUS", "RUS", 190);
+    private Ticket t6 = new Ticket(6, 15000, "UR", "UE", 240);
+    private Ticket t7 = new Ticket(7, 17000, "UMK", "UA", 720);
+    private Ticket t8 = new Ticket(8, 16000, "UO", "UE", 130);
+    private Ticket t9 = new Ticket(9, 14000, "UE", "UO", 190);
 
     @BeforeEach
     void setUP() {
-//        manager.add(t0);
-//        manager.add(t1);
-//        manager.add(t2);
+        manager.add(t0);
+        manager.add(t1);
+        manager.add(t2);
         manager.add(t3);
         manager.add(t4);
         manager.add(t5);
         manager.add(t6);
         manager.add(t7);
-//        manager.add(t8);
+        manager.add(t8);
         manager.add(t9);
     }
 
     @Test
-    void findAll() {
+    void findAllFromLessons() {
 
-        Ticket[] expected = new Ticket[]{t3, t4, t5, t6, t7, t9};
-        Ticket[] actual = new Ticket[]{t7, t6, t9, t5, t4, t3};
+        Ticket[] expected = new Ticket[]{t3, t4, t5, t9, t6, t7};
+        Ticket[] actual = new Ticket[]{t3, t4, t5, t6, t7, t9};
+        Arrays.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void findAllAp() { //сортировка по точке отправления и прибытия
+
+        Ticket[] expected = new Ticket[]{t4, t5};
+        Ticket[] actual = manager.findAll("UW","UMK");
+        Arrays.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void findAllOneAp() { //сортировка по точке отправления и прибытия для одного аэропорта
+
+        Ticket[] expected = new Ticket[]{t9};
+        Ticket[] actual = manager.findAll("UO","UE");
+        Arrays.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void findAllNotFound() { //сортировка по точке отправления и прибытия для одного аэропорта
+
+        Ticket[] expected = new Ticket[]{};
+        Ticket[] actual = manager.findAll("UMK","UE");
         Arrays.sort(actual);
         assertArrayEquals(expected, actual);
     }
