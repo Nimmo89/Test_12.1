8ed9ba262346a8ef34d9932774209a9f7e88de5f
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-09-01 22:25:07 +0700

    Update TicketManager.java

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -3,36 +3,14 @@ package ru.netology.manager;
 import ru.netology.domain.Ticket;
 import ru.netology.reposytory.TicketRepo;
 
+import java.util.Arrays;
+
 public class TicketManager {
     private TicketRepo repository;
 
     public void add(Ticket ticket) {
         repository.save(ticket);
     }
-
-//    public boolean matches(Ticket ticket, String search) {
-//        if (ticket instanceof Ticket) { // если в параметре ticket лежит объект класса Ticket
-//            Ticket ticket = (Ticket) ticket; // положем его в переменную типа Ticket чтобы пользоваться методами класса Ticket
-//            if (ticket.getApIn().contains(search) & ticket.getApOut().contains(search)) { // проверим есть ли поисковое слово в данных об аэропорте
-//                return true;
-//            }
-//            if (ticket.getApOut().contains(search)) {
-//                return true;
-//            }
-//            return false;
-//        }
-//        if (ticket instanceof Ticket) { // если в параметре product лежит объект класса Smartphone
-//            Ticket ticket = (Ticket) product; // положем его в переменную типа Smartphone чтобы пользоваться методами класса Smartphone
-//            if (ticket.getProducer().contains(search)) { // проверим есть ли поисковое слово в данных об производителе
-//                return true;
-//            }
-//            if (ticket.getName().contains(search)) {
-//                return true;
-//            }
-//            return false;
-//        }
-//        return false;
-//    }
 
     public Ticket[] findAll(String from, String to) {
         Ticket[] result = new Ticket[0];
@@ -46,6 +24,7 @@ public class TicketManager {
                 result = tmp;
             }
         }
+        Arrays.sort(result);
         return result;
     }
 
